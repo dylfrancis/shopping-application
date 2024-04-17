0354c7824f3a423f541861a55b0d65c79f85876d
Keenan Herbe <herbekm@mail.uc.edu>
2024-04-16 22:35:21 -0400

    Initial Keenan Customer Changes

---
+++ src/pages/customer/account/index.tsx
@@ -0,0 +1,12 @@
+import { Center } from '@chakra-ui/react';
+import Head from 'next/head';
+import { api } from '~/utils/api';
+
+export default function Account(){
+    return(
+        <div>
+            Account Details:
+        </div>
+
+    );
+}
